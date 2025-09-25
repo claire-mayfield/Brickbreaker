5884f10b97a019d58f4d6e1a75066254f62b0718
claire-mayfield <claire_mayfield@icloud.com>
2025-09-25 11:56:07 -0400

    yay the game is really coming together

--- Brickbreaker/Assets/Scripts/BallBehavior.cs
+++ Brickbreaker/Assets/Scripts/BallBehavior.cs
@@ -2,29 +2,44 @@ using UnityEngine;
 
 public class BallBehavior : MonoBehaviour
 {
-    public float Speed = 5.0f;
+    [SerializeField] private float _launchForce = 5.0f;
+    [SerializeField] private float _paddleInfluence = 0.4f;
 
-    private int _xDirection;
-    private int _yDirection;
+    private Rigidbody2D _rb;
 
     void Start()
     {
-        if (Random.value >= 0.5f)
-        {
-            _xDirection = 1;
-        }
-        else
-        {
-            _xDirection = -1;
-        }
+        _rb = GetComponent<Rigidbody2D>();
 
-        _yDirection = Random.value >= 0.5f ? 1 : -1;
+        Vector2 direction = new Vector2(
+            GetNonZeroRandomFloat(),
+            GetNonZeroRandomFloat()
+        ).normalized;
 
+        _rb.AddForce(direction * _launchForce, ForceMode2D.Impulse);
     }
 
-    void Update()
+    void OnCollisionEnter2D(Collision2D other)
     {
-        transform.position += new Vector3(Speed, Speed, 0.0f) * Time.deltaTime * _xDirection;
+            if (!Mathf.Approximately(other.rigidbody.linearVelocity.y, 0.0f))
+            {
+                Vector2 direction = _rb.linearVelocity * (1 - _paddleInfluence)
+                + other.rigidbody.linearVelocity * _paddleInfluence;
 
+                _rb.linearVelocity = _rb.linearVelocity.magnitude * direction.normalized;
+            }
     }
+    float GetNonZeroRandomFloat(float min = -1.0f, float max = 1.0f)
+    {
+        float num;
+
+        do
+        {
+            num = Random.Range(min, max);
+        } while (Mathf.Approximately(num, 0.0f));
+
+        return num;
+        
+    }
+
 }
---
+++ Brickbreaker/Assets/Scripts/BrickBehavior.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class BrickBehavior : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
